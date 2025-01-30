2e4e76b2d01fbadd241f99b5a1e71f06c8f033c3
zc789 <176691766+zc789@users.noreply.github.com>
2025-01-30 16:26:27 -0500

    Finished human and computer choice

---
+++ java.js
@@ -0,0 +1,34 @@
+//Create function named getComputerChoice
+/*
+Write code so function will randomly return one of the following string values
+"rock", "paper", "scissors".
+*/
+//Create constant with the options of rock, paper, scissors
+
+const options = ["rock", "paper", "scissors"];
+
+function getComputerChoice() {
+    const choice = options[Math.floor(Math.random() * options.length)]; //create const choice so that computer can make a choice while using the options const. use Math.random() to return randome number between 0 and 1 but bc we have 3 elements. multiply it by options.length. then use Math.floor() since we might get 1.5 but we don't have that so we need to round it down 
+    return choice; //return is like console.log that shows what's result
+}
+
+
+console.log(getComputerChoice());
+//Create function that returns human player choice using loop to get player input if doenst, it will keep asking
+
+function getHumanChoice() {
+    let humanInput = false;
+    while(humanInput == false){         //start of loop, so while humanInput is still false it will keep looping
+        const choice = prompt("Rock Paper Scissors"); //use prompt to get user input
+        if(choice == null){         //to check if the prompt is empty
+            continue;           //if it's empty it will continue(loops again)
+        }
+        const choiceLowerCase = choice.toLowerCase();           //have everything the same but forcing lowercase(using toLowerCase()) to whatever the user inputs
+        if(options.includes(choiceLowerCase)){          //use .includes() to validate that the users input is within the choices available
+            humanInput = true;      //if it exist it will be true
+            return choiceLowerCase;     //returns choice in lowercase
+        }
+    }
+}
+
+console.log(getHumanChoice());
